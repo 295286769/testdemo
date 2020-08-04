5e2dab3581fd444b29c1b239d1b1daa0f4fae9f2
huangshang <295286769@qq.com>
2020-08-04 22:16:42 +0800

    添加log

--- app/src/main/java/com/huangshang/mytestdemo/MainActivity.kt
+++ app/src/main/java/com/huangshang/mytestdemo/MainActivity.kt
@@ -2,11 +2,14 @@ package com.huangshang.mytestdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val  TAG="MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"onCreate")
     }
 }
